e650dbd54e5a60262b59dd0e3ffeb78e8d602f9a
Nahum Sá <nahumsaa@gmail.com>
2023-07-14 21:37:26 -0300

    add multi_asset

--- scraping_soccer/scraping_soccer/assets.py
+++ scraping_soccer/scraping_soccer/assets.py
@@ -1,27 +1,22 @@
 import time
+
 import pandas as pd
-from dagster import Output, asset, get_dagster_logger
+from dagster import AssetOut, Output, asset, get_dagster_logger, multi_asset
 
 import scraping_soccer.scrapers.teams as teams
-from scraping_soccer.scrapers import latest_matches
-from scraping_soccer.scrapers import match_statistics
+from scraping_soccer.scrapers import latest_matches, match_statistics
 
 BASE_URL = "https://www.espn.com.br"
 LEAGUE_NAME = "bra.1"
 SEASON = 2023
-SEASON = 2023
 
 logger = get_dagster_logger()
+
 
 @asset
 def teams_info() -> Output:
     teams_df = teams.scrape(LEAGUE_NAME)
-    return Output(
-        value=teams_df,
-        metadata={
-            "num_entries": len(teams_df)
-        }
-    )
+    return Output(value=teams_df, metadata={"num_entries": len(teams_df)})
 
 
 @asset
@@ -30,27 +25,57 @@ def teams_matches(teams_info: pd.DataFrame) -> Output:
 
     for _, (team_id, _) in teams_info.iterrows():
         logger.info(f"Getting latest matches for {team_id}")
-        team_match_df= latest_matches.scrape(team_id=team_id, league_name=LEAGUE_NAME, season=SEASON)
+        team_match_df = latest_matches.scrape(
+            team_id=team_id, league_name=LEAGUE_NAME, season=SEASON
+        )
         all_teams_matches_df = pd.concat([all_teams_matches_df, team_match_df])
         time.sleep(1)
 
-    return Output(value=all_teams_matches_df,
-                  metadata={
-        "num_records": len(all_teams_matches_df),
-        "unique_matches": all_teams_matches_df["match_id"].nunique()
-    })
+    return Output(
+        value=all_teams_matches_df,
+        metadata={
+            "num_records": len(all_teams_matches_df),
+            "unique_matches": all_teams_matches_df["match_id"].nunique(),
+        },
+    )
 
-@asset
-def matches_statistics(teams_matches: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+
+@multi_asset(
+    outs={"extract_matches_statistics": AssetOut(), "extract_matches_info": AssetOut()}
+)
+def extract_matches_statistics_and_info(
+    teams_matches: pd.DataFrame,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
     all_matches_statistics_df = pd.DataFrame()
     all_match_info_df = pd.DataFrame()
 
     for match_id in teams_matches["match_id"].unique():
         logger.info(f"Getting latest matches statistics for {match_id}")
         match_statistics_df, match_info_df = match_statistics.scrape(match_id=match_id)
-        all_matches_statistics_df = pd.concat([all_matches_statistics_df, match_statistics_df])
+        all_matches_statistics_df = pd.concat(
+            [all_matches_statistics_df, match_statistics_df]
+        )
         all_match_info_df = pd.concat([all_match_info_df, match_info_df])
         time.sleep(1)
 
     return all_matches_statistics_df, all_match_info_df
 
+
+@asset
+def matches_statistics(extract_matches_statistics: pd.DataFrame) -> Output:
+    return Output(
+        value=extract_matches_statistics,
+        metadata={
+            "num_records": len(extract_matches_statistics),
+        },
+    )
+
+
+@asset
+def matches_info(extract_matches_info: pd.DataFrame) -> Output:
+    return Output(
+        value=extract_matches_info,
+        metadata={
+            "num_records": len(extract_matches_info),
+        },
+    )
